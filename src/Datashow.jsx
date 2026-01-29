f276199d946e09069844c28c1e00ba345e12585f
sahulakki123 <lakkisahus04@gmail.com>
2026-01-29 15:55:58 +0530

    updata

--- src/Datashow.jsx
+++ src/Datashow.jsx
@@ -1,9 +1,24 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Antigravity from './Library/Antigravity';
 
 const Datashow = () => {
 
+    let [form, setform] = useState({
+            username:"",
+            number:"",
+            productName:"",
+            quantity:"",
+            deliveryAddress:""
+    
+        })
+    
+        let handlchange=(e)=>{
+            setform({ ...form, [e.target.name]: e.target.value })
+        }
+
     let [data, setdata]= useState([])
+    let [editid, seteditid]= useState(null)
 
     let FetchData = ()=>{
         let api = 'http://localhost:3000/protein'
@@ -16,12 +31,6 @@ const Datashow = () => {
           })
           setdata(finaldata)
           
-        })
-
-        axios.get(api).then( (res)=>{
-            console.log(res.data);
-            setdata(res.data)
-            
         }).catch( (err)=>{
             console.log(err);
             
@@ -42,7 +51,28 @@ const Datashow = () => {
 
     }
 
+    let showform=(e)=>{
 
+      seteditid(e.id)
+      setform({
+            username:e.username,
+            number:e.number,
+            productName:e.productName,
+            quantity:e.quantity,
+            deliveryAddress:e.deliveryAddress
+
+      })
+    }
+    let handlesubmit=(e)=>{
+    e.preventDefault()
+    let loggedemail = localStorage.getItem("useremail")
+     let api=`http://localhost:3000/protein/${editid}`
+     axios.put(api, {...form, loggedinuser:loggedemail}).then((e)=>{
+      FetchData()
+      seteditid(null)
+      alert("Data Updated")
+     })
+    }
   return (
     <>
 <div className="p-6">
@@ -57,6 +87,7 @@ const Datashow = () => {
               <th className="p-3 border">Product</th>
               <th className="p-3 border">Qty</th>
               <th className="p-3 border">City</th>
+              <th className="p-3 border">Edit</th>
               <th className="p-3 border">Action</th>
             </tr>
           </thead>
@@ -70,9 +101,8 @@ const Datashow = () => {
                   <td className="p-2 border">{e.productName}</td>
                   <td className="p-2 border">{e.quantity}</td>
                   <td className="p-2 border">{e.deliveryAddress}</td>
-                  <td className="p-2 border"><button onClick={() => Deletedata(e.id)}className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 cursor-pointer">
-                      Delete
-                    </button> </td>
+                  <td className="p-2 border"><button onClick={()=>{showform(e)}} className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 cursor-pointer">Edit</button></td>
+                  <td className="p-2 border"><button onClick={() => Deletedata(e.id)} className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 cursor-pointer">Delete</button> </td>
                 </tr>
               ))
             }
@@ -80,7 +110,68 @@ const Datashow = () => {
         </table>
       </div>
     </div>
+            { editid && (
+              <div className="min-h-screen flex items-center justify-center  from-amber-100 to-orange-200 ml-100%" style={{ width: '100%', height: '400px', position: 'relative', backgroundColor:"black" }}>
+        
 
+                <Antigravity
+    count={300}
+    magnetRadius={6}
+    ringRadius={7}
+    waveSpeed={0.4}
+    waveAmplitude={1}
+    particleSize={1.5}
+    lerpSpeed={0.05}
+    color="#5227FF"
+    autoAnimate
+    particleVariance={1}
+    rotationSpeed={0}
+    depthFactor={1}
+    pulseSpeed={3}
+    particleShape="capsule"
+    fieldStrength={10}
+/>
+
+      
+      <form onSubmit={handlesubmit} className="bg-black p-8 rounded-2xl shadow-xl w-full max-w-md space-y-4">
+
+        <h2 className="text-2xl font-bold text-center text-white">🛒 Update Form</h2>
+
+        <input type="text" name="username" placeholder="Enter Name" required value={form.username} onChange={handlchange} className="w-full p-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-amber-400 text-white"/>
+
+        <input type="text" name="number" placeholder="Enter Mobile Number" value={form.number} onChange={handlchange} className="w-full p-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-amber-400 text-white "/>
+
+        <input type="text" name="productName" placeholder="Enter Product Name" value={form.productName} onChange={handlchange} className="w-full p-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-amber-400 text-white"/>
+
+        <input type="number" name="quantity" placeholder="Enter Quantity" value={form.quantity} onChange={handlchange} className="w-full p-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-amber-400 text-white "/>
+
+        <textarea name="deliveryAddress" placeholder="Enter Delivery Address" value={form.deliveryAddress} onChange={handlchange} className="w-full p-3 border rounded-xl resize-none focus:outline-none focus:ring-2 focus:ring-amber-400 text-white" />
+
+        <button type="submit" className="w-full bg-amber-500 hover:bg-amber-600 text-white font-bold py-3 rounded-xl transition duration-300" >
+        Update Now
+        </button>
+      </form>
+      <Antigravity
+    count={300}
+    magnetRadius={6}
+    ringRadius={7}
+    waveSpeed={0.4}
+    waveAmplitude={1}
+    particleSize={1.5}
+    lerpSpeed={0.05}
+    color="#5227FF"
+    autoAnimate
+    particleVariance={1}
+    rotationSpeed={0}
+    depthFactor={1}
+    pulseSpeed={3}
+    particleShape="capsule"
+    fieldStrength={10}
+/>
+
+              
+    </div>
+            )}
     </>
   )
 }
